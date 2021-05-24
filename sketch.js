745ac6bf23bece49b8f470b96137870deb3b797a
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2021-05-24 22:08:42 +0200

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef, no-unused-vars */
+
+function setup() {
+  createCanvas(400, 400)
+}
+
+function draw() {
+  background(234, 31, 81)
+  noStroke()
+  circle(mouseX, mouseY, 20)
+}
